393ea58350d8e44dadce234545ca5cca0f051a7d
Paul Brown <paul.brown@twi.co.uk>
2022-10-31 09:51:57 +0000

    Update to .net6

--- s4ce/Program.cs
+++ s4ce/Program.cs
@@ -15,6 +15,7 @@ namespace s4ce
 		static void Main()
 		{
 			Application.EnableVisualStyles();
+			//Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new S4CE());
 		}
